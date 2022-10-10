5ef69d6a280c9be119f85e73c3b4cf29689e3b82
Sergey Raklov <ra1976@inbox.ru>
2022-10-10 13:43:47 +0300

    add DifferTest.java

---
+++ app/src/test/java/DifferTest.java
@@ -0,0 +1,19 @@
+import hexlet.code.Differ;
+import org.junit.jupiter.api.Test;
+
+public class DifferTest {
+//    @Test
+//    public void testGenerate() {
+//        String expected = {
+//                - follow: false
+//        host: hexlet.io
+//                - proxy: 123.234.53.22
+//                - timeout: 50
+//                + timeout: 20
+//                + verbose: true
+//};
+//        var actual = Differ.generate();
+//        assertEquals(expected, actual);
+//    }
+
+}
