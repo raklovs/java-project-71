0d7ad4558e6df55152d810356e5e01af94154e02
Sergey Raklov <ra1976@inbox.ru>
2022-10-10 09:56:11 +0300

    add img file

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -12,38 +12,52 @@ import java.util.Set;
 import java.util.TreeSet;
 
 public class Differ {
-    public static String generate(String firstFilePath, String secondFilePath) throws IOException {
-
-        Path path1 = Paths.get(firstFilePath).toAbsolutePath().normalize();// Формируем абсолютный путь
-        String content1 = Files.readString(path1);//Читаем файл
+    public static String generate(String firstFilePath, String secondFilePath) throws Exception {
+        // Формируем абсолютный путь: C:\Users\Sergey\java-project-71\app\src\main\resources\file1.json
+        Path firstAbsolutePathToTheFile = Paths.get(firstFilePath).toAbsolutePath().normalize();
+        Path secondAbsolutePathToTheFile = Paths.get(secondFilePath).toAbsolutePath().normalize();
+        //Читаем файл: {"host": "hexlet.io","timeout": 50,"proxy": "123.234.53.22","follow": false}
+        String firstFileContent = Files.readString(firstAbsolutePathToTheFile);//Читаем файл
+        String secondFileContent = Files.readString(secondAbsolutePathToTheFile);
+        //создаем объект класса ObjectMapper
         ObjectMapper objectMapper = new ObjectMapper();
-        Map<String, Object> map1 = objectMapper.readValue(content1, new TypeReference<>() {
+        //получаем contentKeyValueFirstMap = {host=hexlet.io, timeout=50, proxy=123.234.53.22, follow=false}
+        Map<String, Object> contentKeyValueFirstMap = objectMapper.readValue(firstFileContent, new TypeReference<>() {
         });
-        return "";
+        Map<String, Object> contentKeyValueSecondMap = objectMapper.readValue(secondFileContent, new TypeReference<>() {
+        });
+        Set<String> commonKeys = new TreeSet<>();
+        //Объекты хранятся в отсортированном и возрастающем порядке - commonKeys = [follow, host, proxy, timeout]
+        commonKeys.addAll(contentKeyValueFirstMap.keySet());
+        commonKeys.addAll(contentKeyValueSecondMap.keySet());
+
+        // Варианты условий
+        //1 - ключ есть в обоих файлах, и его значения совпадают - отсутствует плюс или минус(host: hexlet.io)
+        //2 - ключ есть в обоих файлах, и его значения не совпадают - отсутствует плюс или минус(- timeout: 50, + timeout: 20)
+        //3 - ключ присутствует в первом файле, но отсутствует во втором файле - (- follow: false)
+        //4 - ключ отсутствует в первом файле, но присутствует во втором файле - (+ verbose: true)
+        StringBuilder resultOfComparingTwoFiles = new StringBuilder("{\n");
+        for (String key : commonKeys) {
+
+            String str1 = key + ": " + contentKeyValueFirstMap.get(key) + "\n";
+            String str2 = key + ": " + contentKeyValueSecondMap.get(key) + "\n";
+
+            if (contentKeyValueFirstMap.containsKey(key) && !contentKeyValueSecondMap.containsKey(key)) {
+                resultOfComparingTwoFiles.append("- ").append(str1);
+            } else if (contentKeyValueFirstMap.containsKey(key) && contentKeyValueSecondMap.containsKey(key)
+                    && contentKeyValueFirstMap.get(key).equals(contentKeyValueSecondMap.get(key))) {
+                resultOfComparingTwoFiles.append("  ").append(str1);
+            } else if (!contentKeyValueFirstMap.containsKey(key) && contentKeyValueSecondMap.containsKey(key)) {
+                resultOfComparingTwoFiles.append("+ ").append(str2);
+            } else {
+                resultOfComparingTwoFiles.append("- ").append(str1);
+                resultOfComparingTwoFiles.append("+ ").append(str2);
+            }
+        }
+        return resultOfComparingTwoFiles + "}";
     }
 
     public static void main(String[] args) throws IOException {
-        //{
-        //  - follow: false
-        //    host: hexlet.io
-        //  - proxy: 123.234.53.22
-        //  - timeout: 50
-        //  + timeout: 20
-        //  + verbose: true
-        //}
-
-        //{
-        //  "host": "hexlet.io",
-        //  "timeout": 50,
-        //  "proxy": "123.234.53.22",
-        //  "follow": false
-        //}
-
-        //{
-        //  "timeout": 20,
-        //  "verbose": true,
-        //  "host": "hexlet.io"
-        //}
         // Варианты условий
         //1 - ключ есть в обоих файлах, и его значения совпадают - отсутствует плюс или минус(host: hexlet.io)
         //2 - ключ есть в обоих файлах, и его значения не совпадают - отсутствует плюс или минус(- timeout: 50, + timeout: 20)
@@ -83,7 +97,6 @@ public class Differ {
                 result.append("- ").append(str1);
                 result.append("+ ").append(str2);
             }
-
         }
         System.out.println(result + "}");
     }
